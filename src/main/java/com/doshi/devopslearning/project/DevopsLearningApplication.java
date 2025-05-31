07743c6f87aec4e3e7a3be1d41e038d7961dc126
unknown <akshay.doshi94@gmail.com>
2025-05-31 10:02:01 +0530

    Initial Commit

---
+++ src/main/java/com/doshi/devopslearning/project/DevopsLearningApplication.java
@@ -0,0 +1,13 @@
+package com.doshi.devopslearning.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsLearningApplication.class, args);
+	}
+
+}
